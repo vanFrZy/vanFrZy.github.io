2dfd041cbecda16ae0535f31304d8ddbf7fd3b76
vanFrZy <fred.vdw.pro@gmail.com>
2022-12-24 10:03:09 +0100

    Fix: deployment try

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+// const dev = "production" === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,7 +10,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "build",
+			assets: "build"
+		}),
+		paths: {
+			// change below to your repo name
+			base: "",
+		},
+		// hydrate the <div id="svelte"> element in src/app.html
 	}
 };
 
