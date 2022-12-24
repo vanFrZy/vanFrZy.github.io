66dd382fd4c9e604f59561eebe2d12adc4cc56aa
vanFrZy <fred.vdw.pro@gmail.com>
2022-12-24 10:04:05 +0100

    Updates

---
+++ _app/immutable/components/pages/about/_page.svelte-ba9c12e2.js
@@ -0,0 +1,27 @@
+import{S as T,i as j,s as q,k as o,a as F,q as l,a2 as K,l as s,h as c,c as H,m as E,r as m,n as u,X as t,b as C,B as P}from"../../../chunks/index-4fea69f8.js";function L(M){let i,f,a,d,y,b,e,g,w,v,x,I,_,k,S,B,N,A;return{c(){i=o("meta"),f=F(),a=o("div"),d=o("span"),y=l("Welcome reader!"),b=F(),e=o("span"),g=l(`I am about to finish my Master's degree in Computer Science at KTH in Sweden with a specialty in
+		Security and Privacy.
+		`),w=o("br"),v=l(`
+		Feel free to look around my homepage and email or add me on LinkedIn to connect.
+		`),x=o("br"),I=l(`
+		No job applications have been sent out from my side but feel free to headhunt me before I finish
+		my degree.
+		`),_=o("br"),k=l(`
+		New technologies are always fascinating, meaning I can't resist trying them out!
+		`),S=o("br"),B=l(`
+		From a security perspective, there is almost no topic you can bore me with, so feel free to teach
+		me things if you think I don't know them yet, as I am always looking forward to learning new things!
+		`),N=o("br"),A=l(`
+		Have a nice day!!!`),this.h()},l(r){const h=K("svelte-15hgu2d",document.head);i=s(h,"META",{name:!0,content:!0}),h.forEach(c),f=H(r),a=s(r,"DIV",{class:!0});var p=E(a);d=s(p,"SPAN",{class:!0});var R=E(d);y=m(R,"Welcome reader!"),R.forEach(c),b=H(p),e=s(p,"SPAN",{class:!0});var n=E(e);g=m(n,`I am about to finish my Master's degree in Computer Science at KTH in Sweden with a specialty in
+		Security and Privacy.
+		`),w=s(n,"BR",{}),v=m(n,`
+		Feel free to look around my homepage and email or add me on LinkedIn to connect.
+		`),x=s(n,"BR",{}),I=m(n,`
+		No job applications have been sent out from my side but feel free to headhunt me before I finish
+		my degree.
+		`),_=s(n,"BR",{}),k=m(n,`
+		New technologies are always fascinating, meaning I can't resist trying them out!
+		`),S=s(n,"BR",{}),B=m(n,`
+		From a security perspective, there is almost no topic you can bore me with, so feel free to teach
+		me things if you think I don't know them yet, as I am always looking forward to learning new things!
+		`),N=s(n,"BR",{}),A=m(n,`
+		Have a nice day!!!`),n.forEach(c),p.forEach(c),this.h()},h(){document.title="About",u(i,"name","description"),u(i,"content","About me"),u(d,"class","text-center text-4xl pb-5 font-medium"),u(e,"class","text-center text-xl pb-3 font-medium max-w-md"),u(a,"class","flex flex-col pt-9 default-text")},m(r,h){t(document.head,i),C(r,f,h),C(r,a,h),t(a,d),t(d,y),t(a,b),t(a,e),t(e,g),t(e,w),t(e,v),t(e,x),t(e,I),t(e,_),t(e,k),t(e,S),t(e,B),t(e,N),t(e,A)},p:P,i:P,o:P,d(r){c(i),r&&c(f),r&&c(a)}}}class D extends T{constructor(i){super(),j(this,i,null,L,q,{})}}export{D as default};
