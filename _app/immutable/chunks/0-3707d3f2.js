c24607421bf422aee62f89dbb2270b33d8a0843c
vanFrZy <fred.vdw.pro@gmail.com>
2022-12-24 09:24:36 +0100

    Updates

--- _app/immutable/chunks/0-3707d3f2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-06b77bc4.js";export{t as component};
--- _app/immutable/chunks/1-1c895095.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-44f566c8.js";export{t as component};
