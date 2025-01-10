527a167e0ba013f2fd093cea7a6fd66a6e728917
vanFrZy <fred.vdw.pro@gmail.com>
2025-01-10 09:28:06 +0100

    build updates

---
+++ docs/_app/immutable/chunks/0-64dde629.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-f3c4a90a.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-5e060c8f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-754a47ee.js";export{t as component};
